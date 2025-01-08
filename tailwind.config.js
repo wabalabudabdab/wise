733cfc05b523d21edbbc4d97253e860f3a1d66a4
a.terekhova <a.terekhova@ston.fi>
2025-01-08 18:53:20 +0300

    test wh bllock

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    theme: {
+      extend: {
+        fontFamily: {
+          optima: ['Optima', 'sans-serif'],
+        },
+        maxWidth: {
+          '1200': '1200px',
+        },
+        spacing: {
+          '44': '44px',
+        },
+      },
+    },
+    plugins: [],
+  }
+  
